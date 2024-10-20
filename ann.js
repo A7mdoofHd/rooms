97c9d796d95e0ba3708994855bc4b919a12648bc
A7mdoofHd <a7med.mads@gmail.com>
2024-10-21 00:49:20 +0300

    Create ann.js

---
+++ ann.js
@@ -0,0 +1,39 @@
+const shbl_str_users = [{dec: "تست", cls: "shbl_str", icon: "https://up6.cc/2023/03/167949264909021.gif"}];
+setInterval(() => {
+  shbl_str_users.forEach(function (ana) {
+    const _0x260e3c = $("#d2 .uzr .uzr > div:first-child:contains('" + ana.dec + "')");
+    if (_0x260e3c.length) {
+      _0x260e3c.each(function (_0x4b072d, _0x3f84a1) {
+        const _0x4307dc = $(_0x3f84a1).parent().parent();
+        if (!_0x4307dc || _0x4307dc.hasClass(ana.cls)) {
+          return;
+        }
+        _0x4307dc.addClass(ana.cls);
+        _0x4307dc.find(".fitimg.u-pic").append('<img style="max-width: 77px; margin-top: -12px; margin-left: -14px;" class="itar_shbl" title="?????" src="' + ana.icon + '">');
+      });
+    }
+  });
+}, 100);
+$("style").last().append("\n\n\n\n#d2 .uzr.shbl_str .u-topic {\n-webkit-text-fill-color: #0000;\n  \n}\n    #d2 .uzr.shbl_str .fitimg.u-pic {\n        height: 52px !important;\nborder-radius: 100px; !important; \nmargin-bottom: 3px !important;\nmargin-top: 1px !important;\nbox-shadow:inset 0 0 0 rgba(0,0,0,.08),0 0 6px #000;\nfilter: hue-rotate(360deg);\nbox-shadow: 0 0 0px rgb(0 0 0), inset 0 0 4px rgb(0 0 0), 0 0 0 0px #000;\n    }\n    #d2 .uzr.shbl_str .u-msg.break {\n          \nmargin-left: 5px !important;\n   -webkit-background-clip: text;\n  -webkit-text-fill-color: #0000;\n  background-image: linear-gradient(-225deg,#000 0%,#353535 29%,#f00 67%,#000 100%);\n  padding: 0px !important;\n  font-size: 100% !important;\n	\nbackground-size: cover; \n    }\n\n  #d2 .uzr.shbl_str .itar_shbl  {\n        width: 94px;\n        height: 94px;\n        margin-top: -40px;\n        cursor: pointer;\n        margin-left: -55px;\n    }\n\n\n    #d2 .uzr.d-flex.mm.shbl_str {\n      \n  border-radius: 0px 10px 0px 10px;\n   \n  \n\n\n    }\n  \n\n#d2 .uzr.d-flex.mm.hmsg.shbl_str {\n background-image: url(https://up6.cc/2023/03/16794923829062.png);\nbackground-size: 100%;\n border: 1px solid #fff;\nbox-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;\n  margin-bottom: 3px !important;\n  margin-top: 2px !important;\n        background-color: #16202a00 !important;\n   border-radius:  0px 0 0px;\n}\n\n	#d2 .uzr.d-flex.mm.pmsgc.shbl_str {\n background-image: url(https://up6.cc/2023/03/16794923829062.png);\nbackground-size: 100%;\n border: 1px solid #fff;\n box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;\n  margin-bottom: 3px !important;\n  margin-top: 2px !important;\n        background-color: #16202a00 !important;\n}\n\n#room .uzr.shbl_str .btn-primary {\n    border-radius:  0px 10px 0px 10px!important;\n    box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 1px #000;\nborder: 1px solid #fff;\n    background-image: url(https://up6.cc/2023/03/16794923829062.png);\n    background-size: cover;\n    color: #484848;\n\n    font-size: 14px !important;\n-webkit-text-fill-color: #000;\nmargin-left: 4px!important;\n}\n");
+function _0x3843() {
+  const _0x2de5f1 = ["s.ha", "-siz", "cove", "nd-i", "#fff", "255,", "4/05", "3037", ".u-m", "	bac", "size", "m_st", "ding", "-lef", "osel", "//up", " 0 0", "opic", "   }", "png)", "    ", "55, ", "last", "5997249InPANI", "r: #", ": 3p", "an.n", ": #e", "t;\n ", "cls", 'le="', "im_s", "	\n\n ", "colo", "max-", "uzr.", "r.d-", "r;\n ", "1450119wmkWKk", "), i", "tr .", "icon", "\n   ", "hasC", "\n \n\n", "leng", "d2 .", "n-le", " 0 5", "grou", "s://", "aski", "0171", " rgb", "orde", "ساعة", "3!im", "marg", "eigh", ".gif", ";\n  ", ": co", '="اط', "fiti", "rtan", "tant", "ver;", "/up6", "appe", "   h", "ddin", " \n c", "_shb", ";\n\n	", "').m", "13296yzPHbB", "3/11", "rgin", "mage", "u-pi", "olor", ".fit", "1712", "radi", "hado", "8),0", "  \nm", "في ن", "und-", "px r", "back", "reak", "ach", "ps:/", "\n  m", "#d2 ", "2023", 'ss="', 'l" t', " cla", "eft:", " fon", "sg.b", "itar", "r {\n", " src", "t;\nb", "ze: ", "13oLQckn", " -12", "يامر", "nd-s", "  \n ", "argi", "h: 7", "x so", "nd{\n", ": ur", "/202", ".u-t", "impo", "styl", " url", "rder", "441myVpIJ", "8px ", " !im", " 0px", "dec", "\n  b", "000;", "0000", "ٍ جا", "gba(", ".png", "font", "px; ", "100%", "2031", "-pic", " 255", "s: 1", "t;\nm", ";\n\n ", "l(ht", "lass", "185OeMfNL", "w: 0", " \nma", "roun", "0,.0", "/170", "in-t", "http", "بتك ", "186420LbBFVU", "adiu", " pad", "ackg", "ttom", "6813", "x !i", 'px;"', "(0, ", "0px ", "حبَا", "er;\n", ".uzr", "\n	\n\n", "31.p", "d-im", "each", " {\n ", "port", "19919820rKrEPx", " #d2", "1113", "بِاه", "gin-", " -14", " #ff", "\n\n b", "!imp", " mar", "071.", "023/", "ox-s", "nset", " bor", " #00", " 0 r", "addC", ".mm.", "5px ", "nt;\n", "widt", "/11/", "kgro", "find", "n-bo", "tain", "  bo", "50yYxLjP", "56AFPcdd", "0310", ": 0p", "6.cc", "00px", "2px ", "r sp", "e3!i", "ft: ", "d-si", "orta", " .uz", "flex", "2VNQRgB", " 3px", "5205", "; !i", "img.", "73176QTLghb", "t: 8", "px #", "3e3e", "7px;", "er-r", "itle", "ant;", ");\nb", "top:", "px !", "4927", "\n\n\n#", "mpor", "7951", "us: ", "; \nm", "04/1", "\n\n\n ", "tps:", "738343BRLmnM"];
+  _0x3843 = function () {
+    return _0x2de5f1;
+  };
+  return _0x3843();
+}
+(function (_0x46c95e, _0x201452) {
+  const _0x24003e = _0x46c95e();
+  while (true) {
+    try {
+      const _0x155287 = -parseInt(_0x37c8(413, -392)) / 1 * (-parseInt(_0x37c8(388, -511)) / 2) + -parseInt(_0x37c8(569, 536)) / 3 * (-parseInt(_0x37c8(375, 392)) / 4) + parseInt(_0x37c8(560, 1415)) / 5 * (-parseInt(_0x37c8(489, 1361)) / 6) + parseInt(_0x37c8(538, 1374)) / 7 * (parseInt(_0x37c8(393, -567)) / 8) + parseInt(_0x37c8(437, -653)) / 9 + -parseInt(_0x37c8(374, 443)) / 10 * (parseInt(_0x37c8(452, -510)) / 11) + parseInt(_0x37c8(346, -233)) / 12 * (-parseInt(_0x37c8(522, -264)) / 13);
+      if (_0x155287 === _0x201452) {
+        break;
+      } else {
+        _0x24003e.push(_0x24003e.shift());
+      }
+    } catch (_0x2ec61f) {
+      _0x24003e.push(_0x24003e.shift());
+    }
+  }
+}(_0x3843, 449803));
